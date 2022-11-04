968c3d77d6c0911699cc9a97fa0e6a57fa31b59b
Y-7J <jou7yyyy@gmail.com>
2022-11-05 05:05:32 +0900

    一度使ったマーカーを使用できないように ディスプレイの左上をキャプチャ 画面上の0~2のマーカーに応じて読み取りモードを変更するように

--- Media3/main.cpp
+++ Media3/main.cpp
@@ -3,6 +3,8 @@
 #include "sharemem.h"
 #include <Windows.h>
 #include <regex>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 
 using namespace std;
@@ -29,7 +31,66 @@ public:
 	}
 };
 
+/*
+cv::Mat getMat(HWND hWND) {
+	HDC deviceContext = GetDC(hWND);
+	HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
+	
+	RECT windowRect;
+	GetClientRect(hWND, &windowRect);
 
+	int height = windowRect.bottom;
+	int width = windowRect.right;
+
+	HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+	
+	SelectObject(memoryDeviceContext, bitmap);
+	
+	BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+	
+	BITMAPINFOHEADER bi;
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = width;
+	bi.biHeight = -height;
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 1;
+	bi.biYPelsPerMeter = 2;
+	bi.biClrUsed = 3;
+	bi.biClrImportant = 4;
+
+	cv::Mat mat = cv::Mat(height, width, CV_8UC4);
+	return mat;
+	GetDIBits(memoryDeviceContext, bitmap, 0, height, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+	DeleteObject(bitmap);
+	DeleteDC(memoryDeviceContext);
+	ReleaseDC(hWND, deviceContext);
+}
+*/
+
+int marker[200];
+
+int getItemId(int ids) {
+	if (ids >= 10 && ids <= 19) {
+		marker[ids] = 1;
+		return 10;
+	}
+	if (ids >= 20 && ids <= 29){
+		return 20;
+	}
+}
+
+bool idCheck(int ids){
+	if (marker[ids] == 1) {
+		return false;
+	}
+	else {
+		return true;
+	} 
+}
 
 int main(void)
 {
@@ -39,6 +100,43 @@ int main(void)
 
 	MediaImage mediaImage = MediaImage();
 	/**************↑変更しない*******************/
+	
+	//window capture
+	HWND desktop = GetDesktopWindow();
+	RECT rect;
+	GetWindowRect(desktop, &rect);
+	
+	BITMAPINFO bmpInfo;
+	bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bmpInfo.bmiHeader.biWidth = rect.right;
+	bmpInfo.bmiHeader.biHeight = -rect.bottom;
+	bmpInfo.bmiHeader.biPlanes = 1;
+	bmpInfo.bmiHeader.biBitCount = 24; //cv::Matの画像をアルファチャンネル有りにする場合はは32;
+	bmpInfo.bmiHeader.biCompression = BI_RGB;
+	
+	LPDWORD lpPixel;
+	HDC hDC = GetDC(desktop);
+	HBITMAP hBitmap = CreateDIBSection(hDC, &bmpInfo, DIB_RGB_COLORS, (void**)&lpPixel, NULL, 0);
+	HDC hMemDC = CreateCompatibleDC(hDC);
+	SelectObject(hMemDC, hBitmap);
+
+	cv::Mat monitor_img;
+	monitor_img.create(rect.bottom, rect.right, CV_8UC3); //RGBのみ。アルファちゃんねるを加えるにはCV_8UN4
+	
+	// 画像表示用のWindowを作成（ウィンドウの大きさをnative/2に固定する）
+	cv::namedWindow("Screenshot", cv::WINDOW_NORMAL);
+	cv::resizeWindow("Screenshot", GetSystemMetrics(SM_CXSCREEN) / 2, GetSystemMetrics(SM_CYSCREEN) / 2);
+	
+	/*
+	LPCTSTR windowTitle = "MMDAgent - Toolkit for building voice interaction systems";
+	HWND hWND = FindWindow(NULL, windowTitle);
+	while (!hWND) {
+		std::system("cls");
+		std::cout << "Start the MMD..." << std::endl;
+		HWND hwnd = FindWindow(NULL, windowTitle);
+		Sleep(100);
+	}
+	*/
 
 	/**************↓適宜変更する*******************/
 
@@ -56,15 +154,13 @@ int main(void)
 	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
 	// 検出され他マーカーのIDを格納する配列
 	std::vector<int> ids;
+	std::vector<int> mmdIds;
 	// 検出されたマーカーの位置を格納する配列
 	std::vector<std::vector<cv::Point2f>> corners;
+	std::vector<std::vector<cv::Point2f>> mmdcorners;
 
-	//マーカーが複数回検知されないためのフラグ
-	//int flag = 0;
 	//マーカー検知するべきかどうか
 	int markerMode = 0;
-	//printf("type mode 1 or 2\n");
-	//scanf_s("%d", &markerMode);
 
 	//アイテムの宣言 item 名前, id, amount, tier(1|2|3), type(0:数字 1:回復 2:攻撃 3:防御 4:フラグ)
 	//item number0(0, 0, 1, 0, "0");
@@ -84,24 +180,52 @@ int main(void)
 	//item flag1(40, 1, 1, 4, "ItemFlag1");
 	// 
 	//std::regex ClaimMode("SYNTH_START \| mei \| mei_voice_normal \| ここではアイテムを見つけました.*");
-	std::regex ClaimMode("LIPSYNC_EVENT_START\|mei,105,k,50,o,95,d,35,e,90,w,35,a,105,a,110,i,105,t,40,e,85,m,40,u,95,o,115,m,40.*");
-	std::regex UseMode("SYNTH_START\|mei\|mei_voice_normal\|アイテムを使用します.*");
+	//std::regex ClaimMode("LIPSYNC_EVENT_START\|mei,105,k,50,o,95,d,35,e,90,w,35,a,105,a,110,i,105,t,40,e,85,m,40,u,95,o,115,m,40.*");
+	//std::regex UseMode("SYNTH_START\|mei\|mei_voice_normal\|アイテムを使用します.*");
 
 	//std::string event = mmd_camera->event;
 	//event = "";
+	
+	//cv::namedWindow("output", cv::WINDOW_NORMAL);
 
 	while (1) {
 		cv::Mat input;
 		capture >> input;
 
+		BitBlt(hMemDC, 0, 0, rect.right, rect.bottom, hDC, 0, 0, SRCCOPY);
+		GetDIBits(hMemDC, hBitmap, 0, rect.bottom, monitor_img.data, (BITMAPINFO*)&bmpInfo, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+
+		cv::imshow("Screenshot", monitor_img); // スクリーンショットの表示
+		
+		//cv::Mat target = getMat(hWND);
+		//cv::cvtColor(target, target, cv::COLOR_BGR2HSV);
+
+		//cv::imshow("output", target);
 		cv::imshow("input", input);/* カメラより得られた画像inputを画面出力*/
 		//frameからマーカーを検出し、位置をcorners, IDをidsに格納する
 		cv::aruco::detectMarkers(input, dictionary, corners, ids);
+		cv::aruco::detectMarkers(monitor_img, dictionary, mmdcorners, mmdIds);
 		
 		// マーカーの入力を受け付けているか
-		
+		if (mmdIds.size() > 0) {
+			if (mmdIds[0] == 1) {
+				markerMode = 1;
+				printf("Register Mode Enabled\n");
+			}
+			else if (mmdIds[0] == 2) {
+				markerMode = 2;
+				printf("Use Mode Enabled\n");
+			}
+			else if (mmdIds[0] == 0) {
+				markerMode = 0;
+				printf("Marker Mode Disabled\n");
+			}
+		}
 
 		
+
+		/*
+		正規表現でやろうとしたけどうまくいかんかった
 		if (std::regex_match(mmd_camera->event, ClaimMode)) {
 			markerMode = 1;
 			printf("ClaimMode MODE ENABLED\n");
@@ -112,21 +236,29 @@ int main(void)
 			printf("UseMode MODE ENABLED\n");
 			printf("%s\n", mmd_camera->event);
 		}
+		*/
 
 		// アイテム登録
 		if (ids.size() > 0 && markerMode == 1) {
-			markerMode = 0;
 			//マーカーのIDによって処理を分岐
-			switch (ids[0]) {
-			case 10:
-				heal1.amount += 1;
-				printf("%sが追加され%d個になりました\n", heal1.name.c_str(), heal1.amount);
-				break;
+			if (idCheck(ids[0]) == true) {
+				markerMode = 0;
+				switch (getItemId(ids[0])) {
+				case 10:
+					heal1.amount += 1;
+					printf("%sが追加され%d個になりました\n", heal1.name.c_str(), heal1.amount);
+					sprintf_s(mmd_camera->camera, "item_registerd");
+					break;
 
-			case 20:
-				attack1.amount += 1;
-				printf("%sが追加され%d個になりました\n", attack1.name.c_str(), attack1.amount);
-				break;
+				case 20:
+					attack1.amount += 1;
+					printf("%sが追加され%d個になりました\n", attack1.name.c_str(), attack1.amount);
+					sprintf_s(mmd_camera->camera, "item_registerd");
+					break;
+				}
+			}
+			else {
+				printf("Used Marker\n");
 			}
 		}
 
@@ -166,44 +298,11 @@ int main(void)
 					break;
 				}
 			}
-			/*
-			if (ids[0] >= 10 && ids[0] <= 19) {
-				//回復アイテムのamountが１以上であるか
-				if (heal1.amount >= 1) {
-					//itemのamountを減らす
-					heal1.amount--;
-					//mmdにメッセージを送る
-					sprintf_s(mmd_camera->camera, "item_%d", ids[0]);
-					printf("item_used: %d\nleft: %d\n", ids[0], heal1.amount);
-				}
-				else {
-					sprintf_s(mmd_camera->camera, "emptyItem");
-					printf("EMPTY_ITEM %d\n", ids[0]);
-				}
-				markerMode = 0;
-				//検出したマーカーの位置を画面に表示
-				//cv::putText(input, std::to_string(ids[0]), center, cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
-			}
-			else if (ids[0] >= 20 && ids[0] <= 29) {
-				if (attack1.amount >= 1) {
-					//itemのamountを減らす
-					attack1.amount--;
-					//mmdにメッセージを送る
-					sprintf_s(mmd_camera->camera, "item_%d", ids[0]);
-					printf("item_used: %d\nleft: %d\n", ids[0], attack1.amount);
-				}
-				else {
-					sprintf_s(mmd_camera->camera, "emptyItem");
-					printf("EMPTY_ITEM %d\n", ids[0]);
-				}
-				markerMode = 0;
-			}
-			*/
-			//cv::aruco::drawDetectedMarkers(input, corners, ids);
+			cv::aruco::drawDetectedMarkers(input, corners, ids);
 			printf("marker: %d\n", ids[0]);
 		}
 		//printf("%d\n", flag);
-		printf("%s\n", mmd_camera->event);
+		//printf("%s\n", mmd_camera->event);
 
 		int key = cv::waitKey(15);/* キー入力，数値は入力待ち時間(ミリ秒)*/
 
@@ -213,5 +312,8 @@ int main(void)
 	}
 	Closeshare(mmd_camera);
 	cv::destroyAllWindows();
+	ReleaseDC(desktop, hDC);
+	DeleteDC(hMemDC);
+	DeleteObject(hBitmap);
 	return 0;
 }
